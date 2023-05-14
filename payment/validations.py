e7cdb7b3f545554d8e5a8cb3fc1ecad18973fe79
Ramdhan Firdaus Amelia <ramdhan.firdaus@ui.ac.id>
2023-05-14 19:52:54 +0700

    Memperbaiki implementasi

--- payment/validations.py
+++ payment/validations.py
@@ -1,10 +1,7 @@
 import jwt
 from django.conf import settings
 
+
 def cek_login(token, username):
     payload = jwt.decode(jwt=token, key=str(settings.JWT_SECRET_KEY), algorithms=['HS512'], options={"verify_signature": False})
-
-    if username == payload['sub']:
-        return True
-    
-    return False
+    return username == payload['sub']
